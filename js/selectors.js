55d9c43e842fd0484345f87b53cf9740e0089ae4
Marco Burgarello <marco.burgarello@yahoo.com>
2018-06-30 15:38:30 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,12 @@
 // declare your functions here...
+
+$(p)
+
+$('img div:last')
+
+$(ninjaBabySelector)
+
+$(divSelector '.pics')
+
+$(firstListItem 'parent-tag child-tag:first-child alt:pic-list)
+
